5a2d9ee9076aa8668a7eff706f456f3ab5a5d9aa
Danigee <daniav9924@gmail.com>
2022-04-14 17:20:13 -0500

    subi la primera parte del home y el header y el footer

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+//app.use("/static",express.static("public"));
+app.use(express.static("public"));
+
+
+//levantar el server
+app.listen(3040, () =>{ 
+    console.log("server corriendo puerto 3040");
+})
+
+//rutas
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+})
